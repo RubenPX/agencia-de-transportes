3a603a6fd10949dc7213e1fe824c7628cabea00f
RubenPX <rubenmarchante@gmail.com>
2023-01-05 12:02:32 +0100

    ✨ Creamos un parrafo extra

--- index.php
+++ index.php
@@ -12,6 +12,18 @@
     <h1>Esto es un proyecto de ejemplo de momento</h1>
     <img src="imagen_que_no_existe.png" alt="">
     <p>Esto es una beta que no afecta a el proyecto principal</p>
+
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere quae voluptatibus est, magni numquam distinctio
+        saepe blanditiis possimus harum fugiat, ut dolore laborum voluptates. Accusamus autem cum tenetur fuga qui.
+        
+        Illo quasi unde iure iste? Velit nesciunt eos sint sit, expedita quaerat repudiandae facere! Fugiat quis neque
+        consequatur, officia magnam quod. Quas eaque debitis alias dolores cupiditate animi voluptatem possimus.
+
+        Odio quae quasi, possimus ex ipsum doloribus minus. Commodi tempora quae autem perspiciatis laboriosam, quia
+        maiores omnis optio consequuntur, debitis quibusdam consequatur aspernatur repellat. Provident quibusdam ullam
+        laboriosam id quod!
+    </p>
 </body>
 
 </html>
