523922f58fa2748c8f6fa01a150ab54909d06329
RubenPX <rubenmarchante@gmail.com>
2023-01-05 11:27:07 +0100

    ✨ creación de estructura html5 basico

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
-<h1>prueba</h1>
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Proyecto agencia de transportes</title>
+</head>
+
+<body>
+    <h1>Esto es un proyecto de ejemplo de momento</h1>
+</body>
+
+</html>
