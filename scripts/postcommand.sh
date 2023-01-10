198adcbc4d647e79cd84704cb6fd56611f0fe54c
RubenPX <rubenmarchante@gmail.com>
2023-01-10 10:35:29 +0000

    🔨 Redirect apache logs to logs folder

--- scripts/postcommand.sh
+++ scripts/postcommand.sh
@@ -7,8 +7,13 @@ echo "> Link /var/www/html to /src"
 sudo chmod a+x "$(pwd)/src/" && sudo rm -rf /var/www/html && sudo ln -s "$(pwd)/src/" /var/www/html
 
 echo "> link arror.log & access.log to devcontainer folder"
-sudo chmod a+x "$(pwd)/.devcontainer/logs/apache2/" && sudo rm /var/log/apache2/error.log && sudo ln -s "$(pwd)/.devcontainer/logs/apache2/error.log" /var/log/apache2/error.log
-sudo chmod a+x "$(pwd)/.devcontainer/logs/apache2/" && sudo rm /var/log/apache2/access.log && sudo ln -s "$(pwd)/.devcontainer/logs/apache2/access.log" /var/log/apache2/error.log
+sudo chmod a+x "$(pwd)/.devcontainer/logs/apache2/"
+
+sudo rm /var/log/apache2/error.log
+sudo rm /var/log/apache2/access.log
+
+sudo ln -s "$(pwd)/.devcontainer/logs/apache2/error.log" /var/log/apache2/error.log
+sudo ln -s "$(pwd)/.devcontainer/logs/apache2/access.log" /var/log/apache2/access.log
 
 # Enabling module rewrite (Apache config)
 echo "> Enable a2enmod rewrite"
@@ -19,7 +24,8 @@ echo "> composer install"
 composer install
 
 # Start apache server
+echo "> Start Apache"
 apache2ctl start
 
-# postcommand runned
-date >> PostCommandRuns.log
+# Register post command run
+date >> .devcontainer/logs/PostCommandRuns.log
