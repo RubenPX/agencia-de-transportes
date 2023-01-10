a5c7c3d643ce444821f386f4ee9b2ad1efdd8db2
RubenPX <rubenmarchante@gmail.com>
2023-01-10 10:34:59 +0000

    🔧 add `.htaccess` to autoload composer

--- src/index.php
+++ src/index.php
@@ -1,32 +1,5 @@
 <?php
 
-function say($what)
-{
-    echo $what;
-}
+echo "URL: " . $_GET["url"];
 
-?>
-
-<!DOCTYPE html>
-<html lang="es">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Proyecto agencia de transportes</title>
-</head>
-
-<body>
-    <h1>Esto es un proyecto de ejemplo de momento</h1>
-    <img src="imagen_que_no_existe.png" alt="">
-    <p>Esto es una beta que no afecta a el proyecto principal</p>
-
-    <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere quae voluptatibus est, magni numquam distinc
-    </p>
-
-    <p> <?php say("Hello from " . php_uname("r")); ?> </p>
-</body>
-
-</html>
+require_once realpath("../vendor/autoload.php");
