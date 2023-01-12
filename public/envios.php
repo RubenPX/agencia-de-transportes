f9493264a8b4a89029d5cb6276f05f01dc13b2f8
RubenPX <rubenmarchante@gmail.com>
2023-01-12 08:41:51 +0000

    🚧 Add `TODO` to @RubenPX

--- public/envios.php
+++ public/envios.php
@@ -4,6 +4,8 @@ require_once "./shared/blade.php";
 
 $logedUser = "Jhon Doe";
 
+// todo: @RubenPX complete template to use Classes//Envios
+
 echo $blade
     ->view()
     ->make('envios', compact("logedUser"))
