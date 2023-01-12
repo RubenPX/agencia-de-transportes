4af3b106521de4622d53cae2e34b9eaa46fdd23e
RubenPX <rubenmarchante@gmail.com>
2023-01-12 08:31:30 +0000

    ✨ Add function stdObejcts to array

--- src/Converters.php
+++ src/Converters.php
@@ -12,4 +12,17 @@ class Converters {
     public static function objToArray(stdClass $data): array {
         return json_decode(json_encode($data), true);
     }
+
+    /**
+     * Convert stdObjects to arrays
+     * @param array $data
+     * @return array
+     */
+    public static function objsToArray(array $data): array {
+        $func = function ($n) {
+            return Converters::objToArray($n);
+        };
+
+        return array_map($func, $data);
+    }
 }
