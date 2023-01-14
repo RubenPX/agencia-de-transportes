67f71f88e78d729c4b51feb9a95da1979521cf33
RubenPX <rubenmarchante@gmail.com>
2023-01-14 18:21:48 +0000

    ✨ Add repartidor resume

--- www/views/repartidor.blade.php
+++ www/views/repartidor.blade.php
@@ -51,11 +51,26 @@ Panel de repartidor
                     <h3 class="card-title text-center mb-3">Repartos</h3>
 
                     <div class="form-group row">
-                        <label for="completed" class="col-sm-6 col-form-label">Pendientes</label>
+                        <label for="completed" class="col-sm-6 col-form-label">No recogido</label>
                         <div class="col-sm-6">
                             <input type="text" readonly class="form-control-plaintext" id="completed" value="3">
                         </div>
                     </div>
+
+                    <div class="form-group row">
+                        <label for="finished" class="col-sm-6 col-form-label">recogido</label>
+                        <div class="col-sm-6">
+                            <input type="text" readonly class="form-control-plaintext" id="finished" value="5">
+                        </div>
+                    </div>
+
+                    <div class="form-group row">
+                        <label for="finished" class="col-sm-6 col-form-label">en reparto</label>
+                        <div class="col-sm-6">
+                            <input type="text" readonly class="form-control-plaintext" id="finished" value="5">
+                        </div>
+                    </div>
+
                     <div class="form-group row">
                         <label for="finished" class="col-sm-6 col-form-label">Entregados</label>
                         <div class="col-sm-6">
