93d3a7f8c55929d7c5dcdf6c41c2397dc4b81b98
RubenPX <rubenmarchante@gmail.com>
2023-01-16 16:27:01 +0000

    🐛 Fix activo is not required

--- www/views/_crud/client.blade.php
+++ www/views/_crud/client.blade.php
@@ -1,7 +1,13 @@
 @foreach ($properties as $key => $value)
-    @if ($key == "id" || $key == "idPoblacion")
+    @if ($key == "id" || $key == "idPoblacion" || $key == "activo")
         @continue
     @endif
 
     @include("components.forms.input", ["key" => $key, "value" => $value, "readOnly" => $STATE == "VIEW" || $STATE == "DELETE"])
 @endforeach
+
+@include("components.forms.input", [
+    "key" => "activo",
+    "value" => $properties["activo"],
+    "required" => false,
+    "readOnly" => $STATE == "VIEW" || $STATE == "DELETE"])
